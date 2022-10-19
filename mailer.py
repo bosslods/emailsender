6a9980258681778f047b3b44b1bc8b6e776b5a36
yusei <boszlods@gmail.com>
2022-10-19 15:05:57 +0800

    updated mailer.py

--- mailer.py
+++ mailer.py
@@ -1,30 +1,33 @@
 from email import message
 from multiprocessing import context
-from re import S
 import smtplib, ssl
 from email.message import EmailMessage
 from decoder import Z, decoded_creds
 
 print("Simple Python Email Sender")
+choice = ""
 
-port = 465
-SENDER_USERNAME= Z.decod3_user()
-SENDER_PASSWORD = Z.decod3_pass()
+while choice != "N":
+    port = 465
+    SENDER_USERNAME= Z.decod3_user()
+    SENDER_PASSWORD = Z.decod3_pass()
 
-message = EmailMessage()
-message['From'] = SENDER_USERNAME
-message['Subject'] = input("Enter the subject: ")
-message['To'] = input("To: ")
-message.set_content(input("Your Message: "))
+    message = EmailMessage()
+    message['From'] = SENDER_USERNAME
+    message['Subject'] = input("Enter the subject: ")
+    message['To'] = input("To: ")
+    message.set_content(input("Your Message: "))
 
-context = ssl.create_default_context()
+    context = ssl.create_default_context()
 
-print("Sending... Please wait")
+    print("Sending... Please wait")
 
-with smtplib.SMTP_SSL('mail.smtp2go.com', port, context=context) as mailer:
-    mailer.login(SENDER_USERNAME, SENDER_PASSWORD)
-    mailer.send_message(message)
-    
-print("Email succesfully sent")
+    with smtplib.SMTP_SSL('mail.smtp2go.com', port, context=context) as mailer:
+        mailer.login(SENDER_USERNAME, SENDER_PASSWORD)
+        mailer.send_message(message)
+        
+    print("Email succesfully sent")
 
+    choice = input("Wanna try Again? Y/N: ")
 
+print("Thank you!")
